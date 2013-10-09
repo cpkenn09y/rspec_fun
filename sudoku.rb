14945b6c0f402f21ef88f9573d9bfc8131251908
Apprentice <apprentice@dbc13.local>
2013-10-08 19:41:29 -0700

    convert string to array, get row, get column

--- sudoku.rb
+++ sudoku.rb
@@ -1,1 +1,34 @@
+class SudokuSolver
+  attr_reader :board_string
 
+  def initialize(board_string)
+    @board_string = board_string
+    @board = board
+  end
+
+  def board
+    @board_string.split('')
+  end
+
+  def get_row(given_index)
+    row = []
+    @board.each_with_index do |element, index|
+      if index/9 == given_index/9 
+        row << element
+      end
+    end
+  row
+  end
+
+  def get_col(given_index)
+    row = []
+    @board.each_with_index do |element, index|
+      if index % 9 == given_index % 9
+        row << element
+      end
+    end
+  row
+  end
+
+  
+end
--- sudoku_spec.rb
+++ sudoku_spec.rb
@@ -1,3 +1,26 @@
 require 'rspec'
 require_relative "sudoku"
 
+describe "SudokuSolver" do
+  string = "619030040270061008000047621486302079000014580031009060005720806320106057160400030"
+  solver = SudokuSolver.new(string)
+
+  it "takes a string" do
+    expect(solver.board_string).to eq(string)
+  end
+
+  it "turns into an array" do
+    expect(solver.board).to eq(string.split(''))
+  end
+
+  it "get row for specific index" do
+    index = 0
+    expect(solver.get_row(index)).to eq(["6","1","9","0","3","0","0","4","0"])
+  end 
+  
+  it "get column for specific index" do
+    index = 0
+    expect(solver.get_col(index)).to eq(["6","2","0","4","0","0","0","3","1"])
+  end
+end
+
